ccddf22cbe43abe4e5c6c2d3007f88e3ed56ef1c
paulkumpula <n2kupa00@students.oamk.fi>
2023-02-02 11:03:09 +0200

    Added values and consts

--- src/App.js
+++ src/App.js
@@ -2,29 +2,38 @@ import {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [weight, setWeight] = useState(89);
+  const [bottles, setBottles] = useState(3);
+  const [gender, setGender] = useState("male");
+  const [time, setTime] = useState(1);
+  const [result, setResult] = useState(0);
+
+  
+  
+  
   return (
    <>
       <h3>Calculating alcohol blood level</h3>
       <form>
         <div>
           <label>Weight</label>
-          <input name="weight" type="number"></input>
+          <input name="weight" type="number" value={weight} onChange={e =>setWeight(e.target.value)}></input>
         </div>
         <div>
           <label>Bottles</label>
-          <input name="bottles" type="number"></input>
+          <input name="bottles" type="number" value={bottles} onChange={e =>setBottles(e.target.value)}></input>
         </div>
         <div>
           <label>Time</label>
-          <input name="time" type="number"></input>
+          <input name="time" type="number" value={time} onChange={e =>setTime(e.target.value)}></input>
         </div>
         <div>
           <label>Gender</label>
-          <input type="radio" name="gender" value="male" defaultChecked /><label>Male</label>
-          <input type="radio" name="gender" value="female" /><label>Female</label>
+          <input type="radio" name="gender" value="male" defaultChecked onChange={e =>setGender(e.target.value)}/><label>Male</label>
+          <input type="radio" name="gender" value="female" onChange={e =>setGender(e.target.value)}/><label>Female</label>
         </div>
         <div>
-          <output></output>
+          <output>{result.toFixed(0)}</output>
         </div>
         <button>Calculate</button>
 
